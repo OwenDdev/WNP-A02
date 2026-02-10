05745c2e63f42dd02de121e524fc5365c555d011
OwenDdev <porewen3848@conestogac.on.ca>
2026-02-10 12:50:21 -0500

    Server
    Basic Async TCP IP  connection based server created.
    Basic Method that reads the game data files and stores the information in an array.

--- WNP-A02-V1/WNP-A02-V1/obj/Debug/net8.0/WNP-A02-V1.AssemblyInfo.cs
+++ WNP-A02-V1/WNP-A02-V1/obj/Debug/net8.0/WNP-A02-V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WNP-A02-V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5387e0e85236201e0b57187c176099ba73e91317")]
 [assembly: System.Reflection.AssemblyProductAttribute("WNP-A02-V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WNP-A02-V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
